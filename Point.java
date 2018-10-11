97f13bd9dbe8365521277d46d0c574f829a080ac
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-10-10 20:44:47 -0400

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,30 @@
+public class Point{
+  public static void main(String[]args){
+  }
+  private double x,y;
+  public Point(double X, double Y){
+  x = X;
+  y = Y;
+}
+
+public Point(Point p){
+  x = p.x;
+  y = p.y;
+}
+public double getX(){
+  return x;
+}
+
+public double getY(){
+  return y;
+}
+
+public distanceTo(Point p){
+return Math.pow (Math.pow((this.getX() - b.getX()), 2) + Math.pow((this.getY() - b.getY()), 2), 1/2);
+
+}
+
+
+public distance(Point a, Point b){
+return Math.pow (Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2), 1/2);
+}
